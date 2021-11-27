430639ddcc3b6b1dff3ef24369b30ecbe26b04a4
mvs.meah <mvs.meah@yahoo.com>
2021-11-27 23:46:11 +0800

    added signup screen

--- app/src/main/java/com/capstone/pakigsabotbusinessowner/MainActivity.java
+++ app/src/main/java/com/capstone/pakigsabotbusinessowner/MainActivity.java
@@ -2,13 +2,49 @@ package com.capstone.pakigsabotbusinessowner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signin, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        refs();
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signinScreen();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signupScreen();
+            }
+        });
+    }
+
+    public void refs(){
+        signin = findViewById(R.id.signInBtn);
+        signup = findViewById(R.id.signUpBtn);
+    }
+
+    public void signinScreen(){
+        Intent intent = new Intent(getApplicationContext(), SignIn.class);
+        startActivity(intent);
+    }
+
+    public void signupScreen(){
+        Intent intent = new Intent(getApplicationContext(), SignUp.class);
+        startActivity(intent);
     }
 }
