deb774bdaa33ff1237f4fb9a81f6277c38a66df9
mvs.meah <mvs.meah@yahoo.com>
2022-01-14 18:54:00 +0800

    added chatbot conversation layout

--- app/src/main/java/com/capstone/pakigsabotbusinessowner/Chatbot/ChatbotConversation5.java
+++ app/src/main/java/com/capstone/pakigsabotbusinessowner/Chatbot/ChatbotConversation5.java
@@ -2,14 +2,39 @@ package com.capstone.pakigsabotbusinessowner.Chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.capstone.pakigsabotbusinessowner.R;
 
 public class ChatbotConversation5 extends AppCompatActivity {
+
+    TextView homeMenuTxt2CC5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot_conversation5);
+
+        //References:
+        refs();
+
+        homeMenuTxt2CC5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chatbotConversation6();
+            }
+        });
+    }
+
+    public void refs(){
+        homeMenuTxt2CC5 = findViewById(R.id.homeMenuTxt2CC5);
+    }
+
+    private void chatbotConversation6(){
+        Intent intent = new Intent(getApplicationContext(), ChatbotConversation6.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/capstone/pakigsabotbusinessowner/Chatbot/ChatbotConversation6.java
@@ -0,0 +1,15 @@
+package com.capstone.pakigsabotbusinessowner.Chatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.capstone.pakigsabotbusinessowner.R;
+
+public class ChatbotConversation6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot_conversation6);
+    }
+}
