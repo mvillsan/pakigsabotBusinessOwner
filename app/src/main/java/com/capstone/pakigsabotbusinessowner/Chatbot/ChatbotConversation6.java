e2079cba5d6f4b0db9f5a37b090bc124e7e871b5
mvs.meah <mvs.meah@yahoo.com>
2022-01-14 23:53:24 +0800

    added default chatbot convo

--- app/src/main/java/com/capstone/pakigsabotbusinessowner/Chatbot/ChatbotConversation6.java
+++ app/src/main/java/com/capstone/pakigsabotbusinessowner/Chatbot/ChatbotConversation6.java
@@ -2,14 +2,39 @@ package com.capstone.pakigsabotbusinessowner.Chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.capstone.pakigsabotbusinessowner.R;
 
 public class ChatbotConversation6 extends AppCompatActivity {
+
+    TextView homeMenuTxt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot_conversation6);
+
+        //References:
+        refs();
+
+        homeMenuTxt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                defaultChabotConvo();
+            }
+        });
+    }
+
+    public void refs(){
+        homeMenuTxt2 = findViewById(R.id.homeMenuTxt2);
+    }
+
+    private void defaultChabotConvo(){
+        Intent intent = new Intent(getApplicationContext(), ChatbotDefaultConversation.class);
+        startActivity(intent);
     }
 }
