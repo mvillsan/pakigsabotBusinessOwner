0db3d08da637947d3fa614bbb733281381b0697c
mvs.meah <mvs.meah@yahoo.com>
2022-01-27 22:04:09 +0800

    added premium feature layout

---
+++ app/src/main/java/com/capstone/pakigsabotbusinessowner/PremiumApp/GoPremiumWS.java
@@ -0,0 +1,15 @@
+package com.capstone.pakigsabotbusinessowner.PremiumApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.capstone.pakigsabotbusinessowner.R;
+
+public class GoPremiumWS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_premium_ws);
+    }
+}
