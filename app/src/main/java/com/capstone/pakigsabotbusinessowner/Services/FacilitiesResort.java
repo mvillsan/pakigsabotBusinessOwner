58469b7b8af8bcfea8bb9b2d74addd839aa5dc4e
unknown <angeldhanna29@gmail.com>
2022-04-26 20:48:48 +0800

    Added changes to Home Fragment and for Profile with Change Password

---
+++ app/src/main/java/com/capstone/pakigsabotbusinessowner/Services/FacilitiesResort.java
@@ -0,0 +1,14 @@
+package com.capstone.pakigsabotbusinessowner.Services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaiclitiesResort extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faiclities_resort);
+    }
+}
